1a6e52f47f32f8438830e0482df503a2183502a4
emontes <emontes@turista.com.mx>
2020-07-29 16:57:59 -0500

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '16e7b59b56c85a881b11eb79bac44083'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '81a7b6cd-5553-4b71-91c6-1c03861a4a65'
+};
